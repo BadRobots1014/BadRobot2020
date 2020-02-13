a88f5303f13cba2889c1a81f5a2a8f23f58a77b0
Victor Chen <victorchen2022@yahoo.com>
2020-02-13 15:59:24 -0500

    Added autonomous mode chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,9 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -60,7 +63,10 @@ public class RobotContainer {
   private final SparkMaxProvider m_speedControllerProvider;
 
   private final AutoDriveExamplePathCommandGroup m_exampleDrive;
-  private HoldPlaceCommand m_holdPlaceCommand; 
+  private HoldPlaceCommand m_holdPlaceCommand;
+
+  private final ShuffleboardTab m_autonomousShuffleboardTab = Shuffleboard.getTab("Autonomous");
+  private SendableChooser<Command> m_autonomousChooser;
 
 
   /**
@@ -68,6 +74,7 @@ public class RobotContainer {
    */
   public RobotContainer() {
     boolean isReal = Robot.isReal();
+
     m_gyroProvider = new GyroProvider(isReal);
     m_speedControllerProvider = new SparkMaxProvider(isReal);
 
@@ -75,11 +82,13 @@ public class RobotContainer {
     m_gathererSubsystem = new GathererSubsystem(new TalonSRX(34));
     m_feedSubsystem = new FeedSubsystem(new TalonSRX(21));
     m_shooterSubsystem = new ShooterSubsystem();
+
     m_teleopDriveCommand = new TeleopDriveCommand(m_driveTrain);
     m_gatherCommand = new GatherCommand(m_gathererSubsystem);
     m_feedCommand = new FeedCommand(m_feedSubsystem);
     m_holdPlaceCommand = new HoldPlaceCommand(m_driveTrain, m_gyroProvider);
     m_shootCommand = new ShootCommand(m_shooterSubsystem);
+
     // Configure the button bindings
     configureButtonBindings();
     configureDriveTrain();
@@ -87,6 +96,9 @@ public class RobotContainer {
     configureGatherer();
     configureFeeder();
     */
+
+    // Configure SmartDashboard Tabs
+    configureAutonomousTab();
 
     m_exampleDrive = new AutoDriveExamplePathCommandGroup(m_driveTrain);
 
@@ -159,6 +171,17 @@ public class RobotContainer {
   }
   */
 
+  private void configureAutonomousTab()
+  {
+    m_autonomousChooser = new SendableChooser<Command>();
+    m_autonomousChooser.addOption("Hold Place", m_holdPlaceCommand);
+    m_autonomousChooser.addOption("Example Path Drive", m_exampleDrive);
+
+    m_autonomousChooser.setDefaultOption("Hold Place", m_holdPlaceCommand);
+
+    m_autonomousShuffleboardTab.add("Autonomous Chooser", m_autonomousChooser);
+  }
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -166,6 +189,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return m_exampleDrive;    
+    return m_autonomousChooser.getSelected();    
   }
 }
