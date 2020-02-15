789d1a95f48a74c121686c04f6e226db0922501a
SomeoneMysterious <ilovebadgerswill@gmail.com>
2020-02-15 11:14:19 -0500

    bit of color sensor work

--- src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -7,9 +7,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -22,6 +20,7 @@ public class ColorSensorSubsystem extends SubsystemBase {
    * Creates a new ExampleSubsystem.
    */
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
 
   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
@@ -29,12 +28,32 @@ public class ColorSensorSubsystem extends SubsystemBase {
   private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   public ColorSensorSubsystem() {
-    
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
   }
 
-  public char getColor() {
+  public String getColor() {
+    Color detectedColor = m_colorSensor.getColor();
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+    return colorString;
   }
 
-  public int getColorRGB() {
+  public Color getColorRGB() {
+    Color detectedColor = m_colorSensor.getColor();
+    return detectedColor;
   }
 }
